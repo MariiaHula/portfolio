8cd10839c41b8bd64a0b5ce842892b38a9032a04
MariiaHula <mariyagulaya@gmail.com>
2024-01-05 21:57:16 +0200

    added new project and swiper

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,100 +1,93 @@
 import tracker from "../../assets/images/tracker.jpg";
 import phonebook from "../../assets/images/phonebook.jpg";
 import tasty from "../../assets/images/tasty.jpg";
+import movies from "../../assets/images/movies.jpg";
+
 import { Container, Nav } from "react-bootstrap";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
 import css from "./Work.module.css";
 import sprite from "../../assets/sprite.svg";
+import { useState } from "react";
 
 const Work = () => {
+  const [slideData] = useState([
+    {
+      id: "1",
+      image: `${tracker}`,
+      title: "Expenstracker app",
+      description:
+        "The app makes it easy for you to take control of your finances! With intuitive features, it simplifies the process of tracking and managing expenses, allowing you to manage your financial world without stress.",
+      project: "https://nadiyniy.github.io/money_makers/",
+    },
+    {
+      id: "2",
+      image: `${phonebook}`,
+      title: "Phonebook app",
+      description:
+        "The application, with which it is easy to search and quickly save the necessary contacts! Simple search and editing.",
+      project: "https://mariiahula.github.io/Phonebook/",
+    },
+    {
+      id: "3",
+      image: `${movies}`,
+      title: "Movies app",
+      description:
+        "The application is a database of movies. Both general and on-demand navigation, which allows you to quickly find the desired movie. You can also see additional information about the film, cast and reviews.",
+      project: "https://mariiahula.github.io/Movies/",
+    },
+    {
+      id: "4",
+      image: `${tasty}`,
+      title: "Tasty Treats app",
+      description:
+        "A useful application for finding the best recipes. Just save to your favorite recipes and watch a detailed step-by-step guide to cooking. A rating system and the most popular recipes at your fingertips.",
+      project: "https://mariiahula.github.io/js-project-team-2/",
+    },
+  ]);
+
   return (
     <section id="work" className={css.section}>
       <Container>
-        <div className={css.wrapperSection}>
-          <img
-            src={tracker}
-            className="d-block w-50"
-            alt="Expenstracker app"
-            loading="lazy"
-          />
-          <div className={css.workWrapper}>
-            <h1 className={css.titleWork}>Expenstracker app</h1>
-            <p>
-              The app makes it easy for you to take control of your finances!
-              With intuitive features, it simplifies the process of tracking and
-              managing expenses, allowing you to manage your financial world
-              without stress.
-            </p>
-            <Nav.Link
-              className={css.navLink}
-              href="https://nadiyniy.github.io/money_makers/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              View Project
-              <svg className={css.iconArrow}>
-                <use href={`${sprite}#icon-arrow`}></use>
-              </svg>
-            </Nav.Link>
-          </div>
-        </div>
-        <div className={css.border}></div>
+        <Swiper
+          pagination={{
+            dynamicBullets: true,
+          }}
+          loop={true}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          {slideData?.map((app) => (
+            <SwiperSlide key={app.id}>
+              <div className={css.wrapperSection}>
+                <img
+                  src={app.image}
+                  className="d-block w-50"
+                  alt={app.title}
+                  loading="lazy"
+                />
+                <div className={css.workWrapper}>
+                  <h1 className={css.titleWork}>{app.title}</h1>
+                  <p>{app.description}</p>
+                  <Nav.Link
+                    className={css.navLink}
+                    href={app.project}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    View Project
+                    <svg className={css.iconArrow}>
+                      <use href={`${sprite}#icon-arrow`}></use>
+                    </svg>
+                  </Nav.Link>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
 
-        <div className={css.wrapperSection}>
-          <img
-            src={phonebook}
-            className="d-block w-50"
-            alt="Phonebook app"
-            loading="lazy"
-          />
-          <div className={css.workWrapper}>
-            <h1 className={css.titleWork}>Phonebook app</h1>
-            <p>
-              The application, with which it is easy to search and quickly save
-              the necessary contacts! Simple search and editing.
-            </p>
-            <Nav.Link
-              className={css.navLink}
-              href="https://mariiahula.github.io/Phonebook/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              View Project
-              <svg className={css.iconArrow}>
-                <use href={`${sprite}#icon-arrow`}></use>
-              </svg>
-            </Nav.Link>
-          </div>
-        </div>
-        <div className={css.border}></div>
-
-        <div className={css.wrapperSection}>
-          <img
-            src={tasty}
-            className="d-block w-50"
-            alt="Tasty Treads app"
-            loading="lazy"
-          />
-          <div className={css.workWrapper}>
-            <h1 className={css.titleWork}>Tasty Treats app</h1>
-            <p>
-              A useful application for finding the best recipes. Just save to
-              your favorite recipes and watch a detailed step-by-step guide to
-              cooking. A rating system and the most popular recipes at your
-              fingertips.
-            </p>
-            <Nav.Link
-              className={css.navLink}
-              href="https://mariiahula.github.io/js-project-team-2/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              View Project
-              <svg className={css.iconArrow}>
-                <use href={`${sprite}#icon-arrow`}></use>
-              </svg>
-            </Nav.Link>
-          </div>
-        </div>
         <div className={css.border}></div>
       </Container>
     </section>
